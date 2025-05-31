74b80c8e21f4b04c40ea6ba237f6e5802dfc1daf
TomBos <tombos255@gmail.com>
2025-06-01 01:44:48 +0200

    Numpy

--- snn.py
+++ snn.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 def generateNeuron(inputs, weights, bias):
     if len(inputs) != len(weights):
         return {
@@ -5,33 +7,30 @@ def generateNeuron(inputs, weights, bias):
             "message": "Each input doesn't have its own weight!"
         }
 
-    weighted_inputs = []
-
-    for i, input in enumerate(inputs):
-        weighted_inputs.append(input * weights[i])
-
-    return sum(weighted_inputs) + bias
+    # Multiply elementwise each element at the same index for both vectors 
+    # And add it all together
+    return np.dot(weights, inputs) + bias
 
 
 #====== START ======#
 
 neuron_1_inputs = [1,2,3,2.5]
-neuron_2_inputs = [1,2,3,2.5]
-neuron_3_inputs = [1,2,3,2.5]
+neuron_2_inputs = [2.0,5.0,-1.0,2.0]
+neuron_3_inputs = [-1.5,2.7,3.3,-0.8]
 
 
 # This is the importance (weight) of the parameter being passed into the NN
+# Weights change the magnitude of the input impact
 neuron_1_weights = [0.2, 0.8, -0.5, 1]
 neuron_2_weights = [0.5, -0.91, 0.26, -0.5]
 neuron_3_weights = [-0.26, -0.27, 0.17, 0.87]
 
+# Similarly to weights, bias changes the ouput
+# This time bias acts as an offset not as multiplier
 neuron_1_bias = 2
 neuron_2_bias = 3
 neuron_3_bias = 0.5
 
-neuron_1 = generateNeuron(neuron_1_inputs, neuron_1_weights, neuron_1_bias)
-neuron_2 = generateNeuron(neuron_2_inputs, neuron_2_weights, neuron_2_bias)
-neuron_3 = generateNeuron(neuron_3_inputs, neuron_3_weights, neuron_3_bias) 
+output = [ generateNeuron(neuron_1_inputs, neuron_1_weights, neuron_1_bias), generateNeuron(neuron_2_inputs, neuron_2_weights, neuron_2_bias), generateNeuron(neuron_3_inputs, neuron_3_weights, neuron_3_bias) ] 
 
-
-print(neuron_1, neuron_2, neuron_3)
+print(output)
